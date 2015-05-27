fd7e03c31d01b71b4e0647c50066ce304524aaad
Lars-Dominik Braun <lars@6xq.net>
2015-05-27 16:40:49 +0200

    Fix sorting by year

--- data/script.js
+++ data/script.js
@@ -15,7 +15,7 @@ $(document).ready (function () {
 		items.detach ().sort (function (nodeA, nodeB) {
 			var a = $(nodeA).data (by);
 			var b = $(nodeB).data (by);
-			if (by == 'rank') {
+			if (by == 'rank' || by == 'year') {
 				a = parseFloat (a);
 				b = parseFloat (b);
 				return a > b ? 1 : (a < b ? -1 : 0);
